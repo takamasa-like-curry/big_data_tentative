37505f3289e20db27d240da5dd312e52de80d4a3
takamasa-like-curry <sugaharatakamasa@sugawaratakamasanoMacBook-Pro.local>
2023-03-28 11:29:27 +0900

    detail.htmlを作成しました。

--- src/main/java/com/example/controller/ShowDetailController.java
+++ src/main/java/com/example/controller/ShowDetailController.java
@@ -1,17 +1,29 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Item;
+import com.example.service.ShowDetailService;
 
 @Controller
 @RequestMapping("/detail")
 public class ShowDetailController {
+	
+	@Autowired 
+	private ShowDetailService service;
+	
 
 	@GetMapping("")
-	public String showDetail() {
+	public String showDetail(Model model, Integer itemId) {
+		
+		Item item = service.showDetail(itemId);
+		model.addAttribute("item",item);
 		
 		
-		return "";
+		return "detail";
 	}
 }
